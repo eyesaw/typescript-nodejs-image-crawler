7dd8e5da78933e7f3ea9ac5d1bd405bfe4c3cc66
eyesaw <eyesaw666@gmail.com>
2018-12-17 22:11:22 +0100

    fixing the async bug

--- app.ts
+++ app.ts
@@ -1,9 +1,13 @@
 import File_Crawler from './src/File_Crawler';
 
- var kek = new File_Crawler({
+ var file_crawler = new File_Crawler({
   url:'https://www.newgrounds.com/',
   directory:'art',
-  query:'https://art.ngfiles.com/thumbnails',
+  file_pattern:'https://art.ngfiles.com/thumbnails',
   query_type:'img_src',
   count:1,
 });
+
+file_crawler.init().then(
+  () => console.log( file_crawler.output() )
+);
--- src/File_Crawler.ts
+++ src/File_Crawler.ts
@@ -5,54 +5,46 @@ import * as Promise from 'promise';
 interface Config {
   url:string;
   directory:string;
-  query:string;
+  file_pattern:string;
   query_type:string;
   count:number;
 }
 
 export default class File_Crawler
 {
-    private image_source_list = [];
-    private DOM_body:any;
+    private image_source_list:string[] = [];
 
-    constructor( private config:Config )
-    {
-      let _ = this;
-
-      this.request_source_code( _.config.url + _.config.directory ).then(
-        () => _.parse()
-      );
-
-    }
+    constructor( private config:Config ){};
 
     /**
       * request the url content
       * @param url
       */
-    private request_source_code( url:string )
+    public init(): any
     {
       let _ = this;
 
       return new Promise((resolve, reject) => {
 
-        new Request( url, function( error, response, body ) {
+        new Request( _.config.url + _.config.directory, function( error, response, body ) {
+
           if( error === null && ( response && response.statusCode == 200 ) ) {
-            _.DOM_body = body;
+            _.parse( body );
             resolve();
 
           } else {
              throw new Error( '[!] Could not retrive url: ' + error );
              reject();
+
           }
         });
-
       });
     }
 
     // search for all images wich fits the query pattern
-    private parse()
+    private parse( body:string )
     {
-      let HTML_document = new Jsdom.JSDOM(this.DOM_body);
+      let HTML_document = new Jsdom.JSDOM( body );
 
       switch( this.config.query_type )
       {
@@ -61,7 +53,7 @@ export default class File_Crawler
 
           for( let i = 0; i < HTML_images_elements.length; i++ ) {
               // validate
-              if( this.config.query != '' || this.validate(HTML_images_elements[i].src ) ){
+              if( ( !this.config.file_pattern ) || this.validate(HTML_images_elements[i].src ) ){
                   // add to list
                   this.image_source_list.push( HTML_images_elements[i].src );
               }
@@ -72,14 +64,12 @@ export default class File_Crawler
         case 'regex':
           break;
       }
-
-       this.output()
     }
 
     // validate the strings
     private validate( url:any )
     {
-      if ( url.includes( this.config.query ) ) {
+      if ( url.includes( this.config.file_pattern ) ) {
         return url;
       } else {
         return false;
@@ -89,14 +79,14 @@ export default class File_Crawler
     // return the requestet image/s by count
     public output()
     {
-      console.log( this.image_source_list );
-
       if( this.image_source_list ){
         if(this.image_source_list.length < this.config.count ){
             return this.image_source_list;
         } else {
             return this.image_source_list.slice(0, this.config.count);
         }
+      } else {
+        throw new Error( '[!] Could not recive anything' );
       }
     }
 }
